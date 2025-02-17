52b339ff82f0b59993fbda6a9c8ad5f9dec4ef55
Arjun Khetia <arjun@perceptioncare.com>
2025-02-17 14:22:52 +0530

    PostgreSQL Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', // Replace with your PostgreSQL username
+      password: 'password', // Replace with your PostgreSQL password
+      database: 'nest_crud', // Replace with your database name
+      autoLoadEntities: true,
+      synchronize: true, // Set to false in production
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
